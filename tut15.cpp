5d57a08f63f20e0aa8b4330746e9f5a4d0f9fa3f
SACHIN GIRAWALE <girawalesachin2021@gmail.com>
2024-06-14 09:27:29 +0530

    Functions & Function Prototypes in C++

--- tut15.cpp
+++ tut15.cpp
@@ -1,7 +1,30 @@
  #include <iostream>
  using namespace std;
  
+// //defining a function
+//  int sum(int a, int b){
+//    int  c = a+b;
+//      return c;
+//  }
+
+// function prototype
+// type function-name (arguments);
+int sum(int a, int b); //--> Acceptable
+// int sum(int a, b); //--> Not Acceptable
+int sum(int , int ); //--> Acceptable
+
+
+
  int main(){
-    
+    int num1, num2;
+    cout<<"Enter the first number"<<endl;   
+    cin>>num1;
+    cout<<"Enter the second number"<<endl;
+    cin>>num2;
+    cout<<"The sum is "<<sum(num1, num2)<<endl;
     return 0;
+ }
+  int sum(int a, int b){
+   int  c = a+b;
+     return c;
  }
