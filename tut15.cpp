aad7f91fc86c81c36faa02c65c2086fbc45e23d5
SACHIN GIRAWALE <girawalesachin2021@gmail.com>
2024-06-14 10:13:40 +0530

    void function

--- tut15.cpp
+++ tut15.cpp
@@ -12,7 +12,8 @@
 int sum(int a, int b); //--> Acceptable
 // int sum(int a, b); //--> Not Acceptable
 int sum(int , int ); //--> Acceptable
-
+ void  g(void); //--> Acceptable
+// void  g(); //--> Acceptable
 
 
  int main(){
@@ -21,10 +22,20 @@ int sum(int , int ); //--> Acceptable
     cin>>num1;
     cout<<"Enter the second number"<<endl;
     cin>>num2;
+    // num1 & num2 are actual parameters
     cout<<"The sum is "<<sum(num1, num2)<<endl;
+//--> Acceptable
+g(); //--> Acceptable
     return 0;
  }
   int sum(int a, int b){
+    // a and &b are formal parameters whuch will accept the values from actual parameters num1 & num2
    int  c = a+b;
      return c;
+     
+ }
+
+ void g(){
+
+    cout<<endl<<"Hello, Good Morning"<<endl;
  }
