3548a33526600f47ca81988037e3e3307f9d4171
SACHIN GIRAWALE <girawalesachin2021@gmail.com>
2024-06-20 10:19:51 +0530

    Inheritance & Its Different Types with Examples in C++

---
+++ tut36.cpp
@@ -0,0 +1,22 @@
+// Inheritance & Its Different Types with Examples in C++
+// Program to demonstrate the concept of Inheritance in C++ Programming
+// **** reusability of code ****
+// reusing classes saves time and effort ,money and resources
+// The concept of Reusability in C++ is supported using Inheritance
+// We can reuse the properties of an existing class by inherititing from it
+// The existing class is called as the Base Class
+// The new class which is inherited is called as the Derived Class
+// Reusing classes saves time and money
+// There are different types of inheritance in C++
+// 1. Single Inheritance
+// EX. class B: public ----A EK bap ek beta
+
+// 2. Multiple Inheritance
+// EX. class C: public A, public B -----maa-baap ek beta
+
+// 3. Hierarchical Inheritance
+// EX. class B: public A, class C: public A  ----ek bap do bete
+// 4. Multilevel Inheritance
+// EX. class B: public A, class C: public B ----ek bap ek beta ek pota
+// 5. Hybrid Inheritance
+// EX. class D: public A, public B, public C  ----ek bap do bete ek pota
