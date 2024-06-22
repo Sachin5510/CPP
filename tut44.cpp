cb37e2aa19f1143de78c767fe459008d82cd9c30
SACHIN GIRAWALE <girawalesachin2021@gmail.com>
2024-06-22 23:41:49 +0530

    Virtual Base Class in C++

---
+++ tut44.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std; 
+class A { 
+public: 
+    void say() 
+    { 
+        cout << "Hello world"<<endl; 
+    } 
+}; 
+class B : public virtual A { 
+};   
+class C : public virtual A { 
+};   
+class D : public B, public C { 
+}; 
+
+int main(){
+    D a;
+    a.say();
+
+    return 0;
+}
