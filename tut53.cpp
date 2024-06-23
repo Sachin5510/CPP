09f140640c07c898db346be5547ea1fde02ca6ee
SACHIN GIRAWALE <girawalesachin2021@gmail.com>
2024-06-23 22:41:55 +0530

    this Pointer in C++

--- tut53.cpp
+++ tut53.cpp
@@ -12,6 +12,7 @@ class A{
         }
 };
 int main(){
+    //this is a keyword which is a pointer which points to the object which invokes the member function
     A a;
     a.setData(4);
     a.getData();
