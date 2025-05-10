c263467b1949c3ebac218ca92a5f869f5abf7eb3
SACHIN GIRAWALE <girawalesachin2021@gmail.com>
2025-05-10 22:31:46 +0530

    binary search

---
+++ binary_serach.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarys(int nums[], int n, int key)
+{
+
+  int start = 0;
+  int end = n - 1;
+
+  //int mid = (start+end)/2;  // it will possible for small values 
+  int mid = start + ((end-start)/2);
+
+  while (start <= end)
+  {
+    if (nums[mid] == key)
+    {
+      return mid;
+    }
+// right part
+    if (key > nums[mid])
+    {
+      start = mid + 1;
+    }
+    else
+    {
+      end = mid - 1;
+    }
+   mid = (start+end)/2;
+    mid = start + ((end-start)/2);
+  }
+  return -1;
+}
+
+int main()
+{
+
+  int arr[5] = {1, 3, 5, 8,9};
+
+  int index = binarys(arr, 5, 9);
+  cout << "the key index of 8 is " << index;
+  return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,1 @@
-int j=1;
-    while (j<=n)
-    {
-      cout<<j;
-      j++;
-    }
+8
