8168f507790ec2d92aea418360278eea4421aee8
SACHIN GIRAWALE <girawalesachin2021@gmail.com>
2025-05-11 17:08:24 +0530

    ok

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -20,11 +20,28 @@ void isort(vector<int> &v, int n)
         break;
       }
     }
+    v[j + 1] = temp;
   }
+}
+void printa(vector<int> &v, int n)
+{
+
+  for (int i = 0; i < n; i++)
+  {
+
+    cout << v[i] << " ";
+  }
+  cout << endl;
 }
 
 int main()
 {
+
+  vector<int> v = {12, 3, 4, 2, 6, 1};
+  // vector<int> v = {1,2,3,4,5};
+
+  isort(v, 5);
+  printa(v, 5);
 
   return 0;
 }
