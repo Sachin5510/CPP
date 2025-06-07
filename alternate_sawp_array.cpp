bc54e3a874c6d83cb590d4a2c0d6aa366ff7673b
SACHIN GIRAWALE <girawalesachin2021@gmail.com>
2025-06-07 10:11:03 +0530

    first commit

--- alternate_sawp_array.cpp
+++ alternate_sawp_array.cpp
@@ -3,15 +3,27 @@ using namespace std;
 
  void alternate(int nums[], int n){
 
-  int s= 0;
-  int al= s+1;
+  int s= 0; //
+  int al= s+1; 
   int end = n-1;
-  while(s<=end){
+  while(s<=end)  {
 
     swap(nums[s], nums[al]);
     s++;
     end--;
   }
+
+// other way to solve it 
+// Function to swap every pair of alternate elements
+// void swapAlternateElements(int arr[], int length) {
+//     // i goes from 0 to length-2 with a step of 2 (to access pairs)
+//     for (int i = 0; i < length - 1; i += 2) {
+//         swap(arr[i], arr[i + 1]);
+//     }
+// }
+
+
+  
 
 }
 
---
+++ second_largest.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        if(n==0 || n==1){
+            return -1;
+        }
+        
+        int max= INT_MIN;
+        int smax = INT_MIN;
+        for(int i=0;i<n;i++){
+            
+           if(arr[i]>max){
+               smax= max;
+               max= arr[i];
+           }
+           
+           else if(arr[i] >smax && arr[i] !=max){
+               smax = arr[i];
+           }
+        }
+        
+        if(smax== INT_MIN){
+            return -1;
+        }
+     else{
+         return smax;
+     }
+    }
+};
+
+int main(){
+  
+  return 0;
+}
